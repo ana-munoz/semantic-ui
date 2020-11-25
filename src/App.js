b47cd80c18fc34621c4e22d42847212c19028157
amunoz <ana.munoz@alumnos.uv.cl>
2020-11-25 00:19:21 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import { Image} from 'semantic-ui-react';
 //import MyModal from './MyModal';
 import { MenuExamplePointing } from './MenuExamplePointing';
 //import { Menu_test } from './Menu_test';
